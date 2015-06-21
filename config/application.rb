5020a6f353d976486144c0d397c980c67d016825
Dawn Miceli <dawnmiceli100@yahoo.com>
2015-06-21 12:55:44 -0400

    flaticons for heroku

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module TastingNotes
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
